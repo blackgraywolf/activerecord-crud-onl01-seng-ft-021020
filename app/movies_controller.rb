25a5545f441eab2e331780d955b3942ef7510206
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-04-08 02:04:19 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -36,8 +36,8 @@ def can_get_size_of_the_database
   Movie.all.size
 end
 
-def can_find_the_first_item_from_the_database_using_id
-  Movie.find(1).title
+def can_find_the_first_item_from_the_database_using_id(k = 1)
+  Movie.find(k).title
   binding.pry
 end
 

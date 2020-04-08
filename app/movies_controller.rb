d301698f32a13ae407400d3035161fb8717ace8b
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-04-08 01:38:16 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -21,7 +21,7 @@ def can_be_created_in_a_block(args = { title: "Home Alone", release_date: 1990 }
   Movie.create do |m|
     m.title = "Home Alone"
     m.release_date = 1990
-    # binding.pry
+  binding.pry
   end
 end
 

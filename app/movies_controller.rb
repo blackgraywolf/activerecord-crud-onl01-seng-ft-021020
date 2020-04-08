bfe2ce59bb625467f29bad46250fe28b8340fff3
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-04-08 01:38:05 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -18,12 +18,12 @@ def can_be_created_with_a_hash_of_attributes
 end
 def can_be_created_in_a_block(args = { title: "Home Alone", release_date: 1990 })
 # def can_be_created_in_a_block
-#   Movie.create do |m|
-#     m.title = "Home Alone"
-#     m.release_date = 1990
-#     # binding.pry
-#   end
-# end
+  Movie.create do |m|
+    m.title = "Home Alone"
+    m.release_date = 1990
+    # binding.pry
+  end
+end
 
 def can_get_the_first_item_in_the_database
   Movie.first.title

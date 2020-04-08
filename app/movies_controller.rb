5bad0266118d7aa5ff77a64c09aa249e65d93075
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-04-08 01:50:17 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -16,9 +16,9 @@ def can_be_created_with_a_hash_of_attributes
   movie = Movie.create(attributes)
 end
 
-def can_be_created_in_a_block
+def can_be_created_in_a_block(args = { title: "Home Alone", release_date: 1990 })
   Movie.create do |m|
-     m = { title: "Home Alone", release_date: 1990}
+     m = args.title
   
     # binding.pry
   end
